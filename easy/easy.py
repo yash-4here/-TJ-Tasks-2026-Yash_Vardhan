0f05658afd1c7d9f5ff0cabac534bf8ff09bcc9a
Yash <216533952+yash-4here@users.noreply.github.com>
2026-08-30 20:17:28 +0530

    Commit easy task

---
+++ easy/easy.py
@@ -0,0 +1,26 @@
+STOP_WORDS = ["the", "is", "at", "which", "and", "to", "a", "an", "for", "my", "i"]
+technical = ["crash","bug","broken","error"]
+billing = ["bill","charged","payment","subscription"]
+
+def classify_message(sentence):
+   if any(word in sentence for word in technical):
+       print("Technical support")
+   elif any(word in sentence for word in billing):   
+       print("Billing support")
+   else:
+       print("General support")
+
+
+
+def clean_and_tokenize(sentence):
+    sentence=sentence.lower()
+    new= " ".join(
+        word for word in sentence.split()
+        if word not in STOP_WORDS
+)
+    print(new)
+    return new
+
+sentence = str(input("Write a sentence to clean and classify it : "))
+classify_message(clean_and_tokenize(sentence))
+classify_message(clean_and_tokenize("the app crashes every time I open it"))
